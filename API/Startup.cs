b5fe3ec04c8aa74fe85abc14ce158a48a5c7158c
Julian0313 <davidgarzonlozano19@outlook.com>
2022-01-12 16:08:35 -0500

    End of section 8

--- API/Startup.cs
+++ API/Startup.cs
@@ -49,7 +49,7 @@ namespace API
             });
             
         }
-
+ 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -66,7 +66,13 @@ namespace API
             app.UseRouting();
             app.UseStaticFiles();
 
-            app.UseCors("CorsPolicy");
+            // app.UseCors("CorsPolicy");
+             app.UseCors(x => x
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
+            app.UseHttpsRedirection();
 
             app.UseAuthorization();
             app.UseSwaggerDocumentation();
